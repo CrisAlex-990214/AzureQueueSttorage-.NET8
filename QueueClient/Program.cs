c32f3eb9df0cd7c6ea0b1b2de38825f32e689801
CrisAlex <cads.14alex@outlook.com>
2023-12-15 15:31:00 -0500

    First commit

---
+++ QueueClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+string queueName = "queue1";
+string connectionString = "";
+var queueClient = new QueueClient(connectionString, queueName);
+
+Console.WriteLine("Press enter to peek messsages");
+Console.ReadKey();
+PeekedMessage[] messages = queueClient.PeekMessages(32);
+foreach (var message in messages) Console.WriteLine(message.Body);
+
+Console.WriteLine("Messages peeked successfully, deleting the queue...");
+queueClient.Delete();
+
+Console.ReadKey();
---
+++ QueueServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Storage.Queues;
+
+string queueName = "queue1";
+string connectionString = "";
+var queueClient = new QueueClient(connectionString, queueName);
+
+Console.WriteLine("Creating queue...");
+queueClient.Create();
+
+Console.WriteLine("Press Enter to Send Messages");
+Console.ReadKey();
+for (int i = 1; i <= 32; i++) queueClient.SendMessage($"Message #{i}");
+Console.WriteLine("Messages sent successfully!");
+
+Console.ReadKey();
